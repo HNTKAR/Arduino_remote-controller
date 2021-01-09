eeaf5ac3e9e069109f991f1f8f91a264bb568db5
kusari-k <18eh501@ms.dendai.ac.jp>
2021-01-09 18:18:20 +0900

    change Upload speed

--- esp.ino
+++ esp.ino
@@ -5,18 +5,31 @@
 #define LED 5
 #define d_time 1000
 
-void setup(){
-    pinMode(LED,OUTPUT);
+void setup()
+{
+    pinMode(LED, OUTPUT);
     Serial.begin(115200);
     Serial.println("");
+    Serial.print("SSID= ");
+    Serial.print(Nname);
+    Serial.print(" Pass= ");
     Serial.println(Npass);
-
+    WiFi.begin(Nname,Npass);
+    Serial.print("Connecting");
+    while (WiFi.status()!=WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("Local IP is ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop(){
-    
-    digitalWrite(LED,LOW);
+void loop()
+{
+    digitalWrite(LED, LOW);
     delay(d_time);
-    digitalWrite(LED,HIGH);
+    digitalWrite(LED, HIGH);
     delay(d_time);
 }
